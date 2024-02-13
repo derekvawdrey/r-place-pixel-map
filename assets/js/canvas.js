75bb995d355509c8765ff9c52006f21282bb456c
Derek Vawdrey <derek.vawdrey@gmail.com>
2024-02-12 20:38:00 -0700

    Added different colors

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,3 +1,5 @@
+var selectedColor = '#000';
+
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('pixelCanvas');
     const ctx = canvas.getContext("2d");
@@ -7,19 +9,33 @@ document.addEventListener('DOMContentLoaded', function () {
     canvas.height = window.innerHeight * ratio;
     ctx.scale(ratio, ratio);
 
-
     const gridSize = 10;
 
-    for(let i = 0; i < canvas.width; i += gridSize) {
-        for(let j = 0; j < canvas.height; j += gridSize) {
+    ctx.strokeStyle = '#ccc';
+    for (let i = 0; i < canvas.width; i += gridSize) {
+        for (let j = 0; j < canvas.height; j += gridSize) {
+            
             ctx.strokeRect(i, j, gridSize, gridSize);
         }
     }
 
-    canvas.addEventListener('click', function(event) {
+    canvas.addEventListener('click', function (event) {
+        ctx.fillStyle = selectedColor;
         const rect = canvas.getBoundingClientRect();
-        const x = Math.floor((event.clientX - rect.left) / gridSize) * gridSize;
-        const y = Math.floor((event.clientY - rect.top) / gridSize) * gridSize;
+        const x = Math.floor((event.clientX - rect.left) / (gridSize * ratio)) * gridSize;
+        const y = Math.floor((event.clientY - rect.top) / (gridSize * ratio)) * gridSize;
         ctx.fillRect(x, y, gridSize, gridSize);
     });
+    
+
+    
+    const colorDivs = document.querySelectorAll('.color');
+    colorDivs.forEach(colorDiv => {
+        colorDiv.addEventListener('click', function() {
+            const hexCode = this.classList[1].split('-')[1];
+            selectedColor = `#${hexCode}`;
+            console.log('Selected Color:', selectedColor);
+        });
+    });
+
 });
