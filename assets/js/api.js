0d8cc59de64e985975a3a8836aeb71ad14aa3a32
derekvawdrey <derek.vawdrey@gmail.com>
2024-02-20 20:31:48 -0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,10 +1,30 @@
+import {Pixel} from "./models/pixel.js"
+
 class Api{
+
+    /**
+     * 
+     * Do an API call to the database to get the PixelMap for the board
+     * 
+     */
+
     async getPixelMap(){
-        // Here we will return a promise for getting the map
-        if(localStorage.getItem("map") !== null){
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(localStorage.getItem("map") !== null){
+                    resolve(localStorage.getItem("map"))
+                }else{
+                    let map = [];
+                    for(width = 0; width < 100; width += 1){
+                        for(height = 0; height < 100; height += 1){
+                            map.append(new Pixel(x,y,0,0,0));
+                        }
+                    }
 
-        }
-
-
+                    localStorage.setItem("map");
+                    resolve(map);
+                }
+            }, 300);
+          });
     }
 }
