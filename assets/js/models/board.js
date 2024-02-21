2c9829691ba9a6afb934a62ee3718db4abf82061
derekvawdrey <derek.vawdrey@gmail.com>
2024-02-20 20:11:28 -0700

    adding classes

---
+++ assets/js/models/board.js
@@ -0,0 +1,10 @@
+class Board{
+    width;
+    height;
+    map;
+    constructor(width,height,map){
+        this.width = width;
+        this.height = height;
+        this.map = map;
+    }
+}
---
+++ assets/js/models/pixel.js
@@ -0,0 +1,14 @@
+class Pixel{
+    x;
+    y;
+    r;
+    g;
+    b;
+    constructor(x,y,r,g,b){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
