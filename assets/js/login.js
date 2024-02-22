216bda30a6b4dff8a2799c7289d00590785d38d6
derekvawdrey <derek.vawdrey@gmail.com>
2024-02-21 20:19:43 -0700

    Adding in login javascript

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    if(isLoggedIn()){
+        let loginLink = document.getElementById("loginLink");
+        let registerLink = document.getElementById("registerLink");
+        if(loginLink){
+            loginLink.classList.add("hidden");
+            registerLink.classList.add("hidden");
+        }
+    }else{
+        let profileLink = document.getElementById("profileLink");
+        profileLink.classList.add("hidden");
+    }
+    
+    let loginSubmit = document.getElementById("login_submit");
+    loginSubmit.addEventListener('click', () => {
+        login();
+    })
+    
+});
+
 async function login(){
-    let response = await Api.handleAuth();
+    let username = document.getElementById("login_name");
+    let password = document.getElementById("login_password");
+
+    let response = await Api.handleAuth(username,password);
 }
 
 function isLoggedIn(){
