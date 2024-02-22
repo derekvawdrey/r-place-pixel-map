fbcc50f5ed2236bbcfc1113a31e4c8d643ba460c
derekvawdrey <derek.vawdrey@gmail.com>
2024-02-21 19:35:22 -0700

    Updating modal javascript to be more dynamic

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,3 +1,18 @@
+
+document.addEventListener('click', function (event) {
+    const targetObject = event.target;
+    const openModalAttribute = 'data-open-modal-id';
+    const closeModalAttribute = 'data-close-modal-id';
+    if (targetObject.hasAttribute(openModalAttribute)) {
+        openModal(targetObject.getAttribute(openModalAttribute));
+    }else if(targetObject.hasAttribute(closeModalAttribute)){
+        closeModal(targetObject.getAttribute(closeModalAttribute));
+    }
+});
+
+
+
+
 const openModal = function (modalId) {
     console.log("Opening Modal ", modalId)
     let modal = document.getElementById(modalId);
@@ -5,18 +20,22 @@ const openModal = function (modalId) {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
 
-    initClosingListener(modalId);
-
-};
-
-const initClosingListener = function(modalId){
-    let overlay = document.getElementById("modal-overlay");
-    overlay.addEventListener("click", closeModal(modalId));
+    // A timeout so that modal doesnt immediately close
+    setTimeout(function(){
+        initClosingListener(modalId);
+    },500);
 }
 
-const closeModal = function (modalId){
+const initClosingListener = function (modalId) {
+    let overlay = document.getElementById("modal-overlay");
+    overlay.addEventListener("click", function() {
+        closeModal(modalId);
+    });
+}
+
+const closeModal = function (modalId) {
     let modal = document.getElementById(modalId);
     let overlay = document.getElementById("modal-overlay");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
 }
