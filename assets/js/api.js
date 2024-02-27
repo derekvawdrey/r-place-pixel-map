f09355c1042317990d8b6bda53284ce41a1c2fb2
derekvawdrey <derek.vawdrey@gmail.com>
2024-02-26 21:11:49 -0700

    added a temporary image generator for the pixel map

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,11 @@
 class Api{
 
+
+    static getRandomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+
     /**
      * 
      * Do an API call to the database to get the PixelMap for the board
@@ -9,21 +15,38 @@ class Api{
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 if(localStorage.getItem("map") !== null){
-                    resolve(localStorage.getItem("map"))
+                    resolve(JSON.parse(localStorage.getItem("map")))
                 }else{
                     let map = [];
-                    for(width = 0; width < 100; width += 1){
-                        for(height = 0; height < 100; height += 1){
-                            map.append(new Pixel(x,y,0,0,0));
+                    let pixelToMap = pixelsForNewMap();
+                    for (let width = 0; width < 100; width += 1) {
+                        map[width] = [];
+                        for(let height = 0; height < 100; height += 1){
+
+                            let newPixel = pixelToMap[width+height*100];
+
+                            map[width][height] = new Pixel(width,height,newPixel.r,newPixel.g,newPixel.b)
                         }
                     }
 
-                    localStorage.setItem("map");
+                    localStorage.setItem("map", JSON.stringify(map));
                     resolve(map);
                 }
             }, 300);
           });
     }
+
+    static async sendPixelToServer(pixel){
+        this.getPixelMap().then((map)=>{
+            if(pixel.x <= 99 && pixel.y <= 99){
+                map[pixel.x][pixel.y] = pixel;
+                localStorage.setItem("map", JSON.stringify(map));
+                console.log(pixel);
+            }
+        })
+    }
+
+
 
     /**
      * Resolves with the user info and puts the user info into local storage
--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,24 +1,25 @@
 var selectedColor = '#000';
 
+var canvas_x = 0;
+var canvas_y = 0;
+var canvas_width = 100;
+var canvas_height = 100;
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('pixelCanvas');
     const ctx = canvas.getContext("2d");
 
     let ratio = window.devicePixelRatio;
+    const gridSize = 5;
     setupCanvasDim();
-    const gridSize = 20;
+    drawCanvas();
 
-    ctx.strokeStyle = '#ccc';
-    for (let i = 0; i < canvas.width; i += gridSize) {
-        for (let j = 0; j < canvas.height; j += gridSize) {
-            
-            ctx.strokeRect(i, j, gridSize, gridSize);
-        }
-    }
+
 
     window.addEventListener('resize', function () {
         setupCanvasDim();
-        redrawGrid();
+        drawCanvas();
     });
 
     function setupCanvasDim() {
@@ -30,25 +31,70 @@ document.addEventListener('DOMContentLoaded', function () {
         ctx.scale(ratio, ratio);
     }
     
-    function redrawGrid() {
+    function drawGrid() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-        const gridSize = 20;
-    
         ctx.strokeStyle = '#ccc';
-        for (let i = 0; i < canvas.width; i += gridSize) {
-            for (let j = 0; j < canvas.height; j += gridSize) {
-                ctx.strokeRect(i, j, gridSize, gridSize);
+        ctx.fillStyle = 'transparent';
+        for (let i = 0; i < canvas_height * gridSize; i += gridSize) {
+            for (let j = 0; j < canvas_width * gridSize; j += gridSize) {
+                ctx.beginPath();
+                ctx.rect(i, j, gridSize, gridSize);
+                ctx.stroke();
             }
         }
     }
 
-    canvas.addEventListener('click', function (event) {
-        ctx.fillStyle = selectedColor;
+    async function drawCanvas(){
+        drawGrid();
+        // Draw pixels
+        let map = await Api.getPixelMap();
+        for (var i = 0; i < map.length; i++) {
+            var row = map[i];
+            for (var j = 0; j < row.length; j++) {
+                var element = row[j];
+                drawPixel(element);
+            }
+        }
+
+        
+    }
+
+    /**
+     * 
+     * TODO: Add future Websocket here
+     */
+    function updateCanvas(){
+        
+    }
+
+    function hexToRgb(hex) {
+        hex = hex.replace(/^#/, '');
+        var bigint = parseInt(hex, 16);
+        var r = (bigint >> 16) & 255;
+        var g = (bigint >> 8) & 255;
+        var b = bigint & 255;
+        return { r, g, b };
+    }
+
+
+    function drawPixel(pixel){
         const rect = canvas.getBoundingClientRect();
-        const x = Math.floor((event.clientX - rect.left) / (gridSize * ratio)) * gridSize;
-        const y = Math.floor((event.clientY - rect.top) / (gridSize * ratio)) * gridSize;
+        const x = Math.floor(pixel.x * gridSize);
+        const y = Math.floor(pixel.y * gridSize);
+
+        ctx.fillStyle = `rgb(${pixel.r}, ${pixel.g}, ${pixel.b})`;    
         ctx.fillRect(x, y, gridSize, gridSize);
+    }
+
+    canvas.addEventListener('click', async function (event) {
+        const x = Math.floor(event.clientX/(gridSize*ratio));
+        const y = Math.floor(event.clientY/(gridSize*ratio));
+        console.log(x,y)
+        const color = hexToRgb(selectedColor);
+        let pixel = new Pixel(x,y,color.r,color.g,color.b);
+        drawPixel(pixel);
+        await Api.sendPixelToServer(pixel);
     });
     
 
