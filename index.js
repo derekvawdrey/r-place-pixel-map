f02079bc14f5db548b8fc5a291a5cf4e2d3b333b
Derek Vawdrey <derek.vawdrey@gmail.com>
2024-03-11 21:07:59 -0600

    Moving more files around

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const http = require('http');
+const app = require('./public/api/app');
+
+const appPort = process.env.Port || 3000;
+app.set('port', appPort);
+
+http.createServer(app).listen(
+    appPort,
+    () => console.log(`Node app running at localhost:${appPort}`);
+    );
