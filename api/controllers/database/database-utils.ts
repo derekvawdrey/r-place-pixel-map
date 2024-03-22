c2b9d364e87b6ded325f1e0f466177fbed1f2152
derekvawdrey <derek.vawdrey@gmail.com>
2024-03-22 16:11:15 -0600

    Creating more util functions

--- api/controllers/database/database-utils.ts
+++ api/controllers/database/database-utils.ts
@@ -3,17 +3,45 @@ const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
+const bcrypt = require('bcrypt');
 
+const userCollection = client.collection("user");
+
+/**
+ * Takes a username and password and if correct returns a User model, if not returns null
+ * @param username 
+ * @param password 
+ * @returns 
+ */
 const auth = (username, password) : User => {
-
     const token = jwt.sign({ username: username }, config.secret_key, { expiresIn: '1m' });
-
+    
     return new User(username, token);
 }
 
+/**
+ * 
+ * @param username 
+ * @param password 
+ * @returns 
+ */
 const register = (username, password) => {
     const token = jwt.sign({ username: username }, config.secret_key, { expiresIn: '1m' });
-
+    // If already exists return 409
     return new User(username, token);
 }
+
+const updateJwtToken = (user:User) => {
+    
+}
+
+/**
+ * 
+ * @param username 
+ * @returns boolean
+ */
+function userExists(username) : boolean {
+    return userCollection.findOne({ username: username });
+}
+
 module.exports = {auth, register};
--- api/controllers/database/user-model.js
+++ api/controllers/database/user-model.js
@@ -1,10 +1,12 @@
 class User {
     username;
     jwt;
+    password;
 
     constructor(username, jwt){
         this.username = username;
         this.jwt = jwt;
+        this.password = password;
     }
 }
 
